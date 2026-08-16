c403e2f557e25b3116ce590da7e4425e86974578
Matheus Freitas <mf022rj@gmail.com>
2026-08-16 00:43:24 -0300

    Add customer service module with initial configuration and setup

---
+++ customer-service/src/test/java/io/matheus/customer/CustomerApplicationTests.java
@@ -0,0 +1,14 @@
+package io.matheus.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class CustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
